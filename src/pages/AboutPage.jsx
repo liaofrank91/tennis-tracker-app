94d880c0e25f27c8a64f001dfee767098a1e9991
Frank Liao <liao.frank.91@gmail.com>
2022-07-05 15:31:45 -0400

    updated About Page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,24 @@ function AboutPage() {
   return (
     <>
       <Navbar />
-      <div>About</div>
+      <br />
+      <br />
+      <br />
+      <section className="flex flex-row justify-center items-center">
+
+
+        <div className='prose'>
+          <h3>
+            A Tennis Tracker App that lets you add verified matches to a watchlist, search for players, and view live rankings.
+          </h3>
+          <h4>Covers both the ATP and WTA circuit.</h4>
+          <h4>Built with React and Firebase (used Firebase Authentication, Firestore)</h4>
+          <ul>API Limitations I've noticed (using Tennis Live Data API from RapidAPI):</ul>
+
+          <li>Player names with variable spelling can sometimes change. e.g. Djokovic, Dokovic</li>
+          <li>Data pertaining to RACE RANKINGS (Race Rank, Race Points) are incorrect/outdated, whereas LIVE RANKING data is fine</li>
+        </div>
+      </section>
     </>
   )
 }
